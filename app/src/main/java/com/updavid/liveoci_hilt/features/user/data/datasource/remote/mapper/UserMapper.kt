d376cbcb19ee0a4c407840b427cba741a97b4c28
dsave-GX4 <angoga344@gmail.com>
2026-04-24 06:01:19 -0600

    Cambio en dto para entidad usuario

--- app/src/main/java/com/updavid/liveoci_hilt/features/user/data/datasource/remote/mapper/UserMapper.kt
+++ app/src/main/java/com/updavid/liveoci_hilt/features/user/data/datasource/remote/mapper/UserMapper.kt
@@ -3,13 +3,15 @@ package com.updavid.liveoci_hilt.features.user.data.datasource.remote.mapper
 import com.updavid.liveoci_hilt.core.database.entities.UserEntity
 import com.updavid.liveoci_hilt.features.user.data.datasource.remote.models.response.UserResponseDto
 
-fun UserResponseDto.toEntity() = UserEntity(
-    id,
-    name,
-    email,
-    notification,
-    interests,
-    topics,
-    description,
-    leisureType
-)
+fun UserResponseDto.toEntity(): UserEntity{
+    return UserEntity(
+        id = this.id,
+        name = this.name,
+        email = this.email,
+        notification = this.notification,
+        interests = this.interests ?: emptyList(),
+        topics = this.topics?: emptyList(),
+        description = this.description,
+        leisureType = this.leisureType
+    )
+}
